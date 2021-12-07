ea4d891622b934ee100ee71aa3d2c7afe1f971f4
haluk.dogan@dmo.gov.tr <haluk.dogan@dmo.gov.tr>
2021-12-07 17:01:43 +0300

    End of section 5

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -7,6 +7,11 @@ import HomePage from "../../features/home/HomePage"
 import ProductDetails from "../../features/catalog/ProductDetails"
 import AboutPage from "../../features/about/AboutPage"
 import ContactPage from "../../features/contact/ContactPage"
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import ServerError from "../errors/ServerError"
+import NotFound from "../errors/NotFound"
+import { Switch } from "react-router-dom";
 function App() {
   const [darkMode, setDarkMode] = useState(false)
   const palleteType = darkMode ? 'dark' : 'light';
@@ -23,14 +28,19 @@ function App() {
   }
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position='bottom-right' hideProgressBar theme='colored'/>
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
+        <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/catalog' component={Catalog}/>
         <Route path='/catalog/:id' component={ProductDetails}/>
         <Route path='/about' component={AboutPage}/>
         <Route path='/contact' component={ContactPage}/>
+        <Route path='/server-error' component={ServerError}/>
+        <Route component={NotFound}/>
+        </Switch>
       </Container>
     </ThemeProvider>
   );
